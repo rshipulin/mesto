bf7939d08730d6818dc23d50964fa5faa627cd37
Roman Shipulin <boogevooge@yandex.ru>
2020-09-05 03:59:53 +0300

    фикс правок после первого ревью

--- script/script.js
+++ script/script.js
@@ -1,40 +1,33 @@
-let popup = document.querySelector('.popup');
-let page = document.querySelector('.page');
-let name = document.querySelector('.profile__name');
-let profession = document.querySelector('.profile__profession');
-let popupOpenButton = document.querySelector('.profile__edit');
-let popupSaveButton = document.querySelector('.popup__submit');
-let popupCloseButton = popup.querySelector('.popup__close');
-let form = popup.querySelector('.popup__form');
-let nameInput = form.querySelector('.popup__input_name');
-let jobInput = form.querySelector('.popup__input_profession');
+let accountPopUp = document.querySelector('.popup');
+let accountEdit = document.querySelector('.profile__edit');
+let accountClose = accountPopUp.querySelector('.popup__close');
+let accountSave = accountPopUp.querySelector('.popup__container');
+let accountInputName = accountPopUp.querySelector('.popup__input_name');
+let accountInputProf = accountPopUp.querySelector('.popup__input_profession');
+let accountName = document.querySelector('.profile__name');
+let accountProfession = document.querySelector('.profile__profession');
 
-let popupToggle = function(event) {
-  event.preventDefault();
-  popup.classList.toggle('popup__opened');
+function popUpToggle(obj) {
+  obj.classList.toggle('popup_opened')
 }
 
-let closePopup = function() {
-  if(event.target !== event.currentTarget) {
-    return;
-  }
-  popupToggle(event);
+function showPopup() {
+  accountInputName.value = accountName.textContent;
+  accountInputProf.value = accountProfession.textContent;
+  popUpToggle(accountPopUp);
 }
 
-function ReadValue(evt) {
+function closePopup() {
+  popUpToggle(accountPopUp);
+}
+
+function accountFormSubmit (evt) {
   evt.preventDefault();
-  nameInput.value = document.getElementsByName('name')['0'].textContent;
-  jobInput.value = document.getElementsByName('job')['0'].textContent;
+  accountName.textContent = accountInputName.value;
+  accountProfession.textContent = accountInputProf.value;
+  popUpToggle(accountPopUp);
 }
 
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  name.textContent = nameInput.value;
-  profession.textContent = jobInput.value;
-}
-
-popup.addEventListener('click', closePopup);
-popupOpenButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
-popupSaveButton.addEventListener('click', formSubmitHandler);
-popupSaveButton.addEventListener('click', closePopup);
+accountEdit.addEventListener('click', showPopup); // открытие попапа
+accountClose.addEventListener('click', closePopup); // закрытие попапа
+accountSave.addEventListener('submit', accountFormSubmit); // отправка формы
