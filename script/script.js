16cd53ce60dbba28c5c6362056cf7b6aa076e770
Roman Shipulin <boogevooge@yandex.ru>
2020-09-08 03:09:13 +0300

    5 проектная работа

--- script/script.js
+++ script/script.js
@@ -1,29 +1,152 @@
-let accountPopUp = document.querySelector('.popup');
-let accountEdit = document.querySelector('.profile__edit');
-let accountClose = accountPopUp.querySelector('.popup__close');
-let accountSave = accountPopUp.querySelector('.popup__container');
-let accountInputName = accountPopUp.querySelector('.popup__form-item_input_name');
-let accountInputProf = accountPopUp.querySelector('.popup__form-item_input_profession');
-let accountName = document.querySelector('.profile__name');
-let accountProfession = document.querySelector('.profile__profession');
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
-function showPopup() {
+const accountPopUp = document.querySelector('.popup[data-type="account"]');
+const accountEditButton = document.querySelector('.profile__edit');
+const accountCloseButton = accountPopUp.querySelector('.popup__close');
+const accountSaveButton = accountPopUp.querySelector('.popup__container');
+const accountInputName = accountPopUp.querySelector('.popup__form-item_input_name');
+const accountInputDesc = accountPopUp.querySelector('.popup__form-item_input_profession');
+
+const accountName = document.querySelector('.profile__name');
+const accountDescription = document.querySelector('.profile__profession');
+
+const placePopUp = document.querySelector('.popup[data-type="place"]');
+const placeAddButton = document.querySelector('.profile__add');
+const placeCloseButton = placePopUp.querySelector('.popup__close');
+const placeSaveButton = placePopUp.querySelector('.popup__container');
+const placeInputTitle = placePopUp.querySelector('.popup__form-item_input_name');
+const placeInputLink = placePopUp.querySelector('.popup__form-item_input_profession');
+
+
+function popUpToggle(obj) {
+  obj.classList.toggle('popup_opened')
+}
+
+function showAccountPopUp() {
   accountInputName.value = accountName.textContent;
-  accountInputProf.value = accountProfession.textContent;
-  accountPopUp.classList.toggle('popup_opened');
+  accountInputDesc.value = accountDescription.textContent;
+
+  popUpToggle(accountPopUp);
 }
 
-function closePopup() {
-  accountPopUp.classList.toggle('popup_opened');
+function closeAccountPopUp() {
+  popUpToggle(accountPopUp);
 }
 
-function accountFormSubmit (evt) {
+function accountFormSubmitHandler (evt) {
   evt.preventDefault();
   accountName.textContent = accountInputName.value;
-  accountProfession.textContent = accountInputProf.value;
-  accountPopUp.classList.toggle('popup_opened');
+  accountDescription.textContent = accountInputDesc.value;
+  popUpToggle(accountPopUp);
 }
 
-accountEdit.addEventListener('click', showPopup);
-accountClose.addEventListener('click', closePopup);
-accountSave.addEventListener('submit', accountFormSubmit);
+accountEditButton.addEventListener('click', showAccountPopUp);
+accountCloseButton.addEventListener('click', closeAccountPopUp);
+accountSaveButton.addEventListener('submit', accountFormSubmitHandler);
+
+
+
+function showPlacePopUp() {
+  popUpToggle(placePopUp);
+}
+
+function closePlacePopUp() {
+  popUpToggle(placePopUp);
+}
+
+function placeFormSubmitHandler (evt) {
+  evt.preventDefault();
+  const newCard = {};
+  newCard.name = placeInputTitle.value;
+  newCard.link = placeInputLink.value;
+  console.log(createCard(newCard))
+  renderCard(createCard(newCard));
+  popUpToggle(placePopUp);
+
+}
+
+placeAddButton.addEventListener('click', showPlacePopUp);
+placeCloseButton.addEventListener('click', closePlacePopUp);
+placeSaveButton.addEventListener('submit', placeFormSubmitHandler);
+
+// лайтбокс
+
+const lightbox = document.querySelector('.lightbox');
+const lightboxCloseBtn = document.querySelector('.lightbox__close');
+
+function addLightbox(card) {
+  const cardImage = card.querySelector('.card__image');
+  cardImage.addEventListener('click',function (event) {
+    const caption = event.target.parentElement.querySelector('.card__name').textContent
+    const imgSrc = event.target.src
+    let lightboxCaption = lightbox.querySelector('.lightbox__caption');
+    const lightboxImage = lightbox.querySelector('.lightbox__image');
+    lightboxCaption.textContent = caption;
+    lightboxImage.src = imgSrc;
+    lightboxImage.alt = caption;
+    toggleFullScreen()
+  })}
+
+
+function toggleFullScreen() {
+  lightbox.classList.toggle('lightbox_opened');
+}
+
+lightboxCloseBtn.addEventListener('click',toggleFullScreen);
+
+// карточки
+
+const cardsContainer = document.querySelector('.cards__items');
+const cardTemplate = document.querySelector('#card').content;
+
+function createCard(array) {
+  const card = cardTemplate.cloneNode(true);
+  card.querySelector('.card__name').textContent = array.name;
+  card.querySelector('.card__image').src = array.link;
+  card.querySelector('.card__image').alt = array.name;
+  card.querySelector('.card__like').addEventListener('click', function (event) {
+    event.target.classList.toggle('card__like_active');
+  })
+  card.querySelector('.card__delete').addEventListener('click', function (event) {
+    event.target.parentElement.remove();
+  })
+  addLightbox(card);
+  return card;
+}
+
+function renderCard(card, method = 'prepend') {
+  if (method === 'prepend') {
+    cardsContainer.prepend(card);
+  } else {
+    cardsContainer.append(card);
+  }
+}
+
+initialCards.forEach(function(element) {
+  renderCard(createCard(element), 'append');
+});
